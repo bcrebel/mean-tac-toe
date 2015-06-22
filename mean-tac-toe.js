41fd770c2288920bc10a59445f30f45d05b87e99
Carron White <carronwhite@gmail.com>
2015-06-22 13:34:42 -0400

    final refactoring

--- mean-tac-toe.js
+++ mean-tac-toe.js
@@ -1,13 +1,11 @@
-/** mean tac toe
+/**********************mean tac toe***********************
 Mean Girls-themed minimax tic tac toe game by Carron White
-*/
+**********************************************************/
  
 
 function Round(el) {
 
-
-
-	 var frame = document.createElement('div'),
+	var frame = document.createElement('div'),
 	 	size = (0.4 * window.innerWidth) + 'px',
 	 	sizeInt = parseInt(size);
 	 	marginCalc = (window.innerWidth ) + '%',
@@ -15,97 +13,116 @@ function Round(el) {
 	 	context = canvas.getContext('2d'),
 	 	board = [0, 0, 0, 0, 0, 0, 0, 0],
 	 	ref = document.getElementsByTagName('h2')[0],
-		comments = ["SO you think you’re really pretty?",
-		 "Shut up!",
-		 "Stop trying to make fetch happen!",
-		  "Vintage, so adorable", 
-		 "You could try Sears",
-		 "That's just the rules of femenism",
-		 "BOO you whore!",
-		 "You CAN'T sit with us!"],
+		comments = [
+			"SO you think you’re really pretty?",
+			"Shut up!",
+			"Stop trying to make fetch happen!",
+			"Vintage, so adorable", 
+			"You could try Sears",
+			"That's just the rules of femenism",
+			"BOO you whore!",
+	 		"You smell like a baby prostitute",
+			"You CAN'T sit with us!"
+		 ],
+		// Store the player images
 	 	sources = {
 	 		cady: 'img/cady.png',
-	 		regina: 'img/regina.png',
+	 		regina: 'img/regina.png'
  	 	},
 	 	turn = 0,
-	 	combos = [[0, 1, 2],
-	 	 [3, 4, 5], 
-	 	 [6, 7, 8], 
-	 	 [0, 3, 6], 
-	 	 [1, 4, 7],
-	 	 [2, 5, 8],
-	 	 [0, 4, 8], 
-	 	 [6, 4, 2]];
+	 	// Winning move combinations
+	 	combos = [
+		 	[0, 1, 2],
+		 	[3, 4, 5], 
+		 	[6, 7, 8], 
+		 	[0, 3, 6], 
+		 	[1, 4, 7],
+		 	[2, 5, 8],
+		 	[0, 4, 8], 
+		 	[6, 4, 2]
+	 	],
+	 	restartRound = function() {
+	 		return setTimeout(function() {
+	 			location.reload();
+	 		}, 1500);
+	 	};;
 	 	
-
-	 
-
-	 frame.id = 'frame';
-	 document.body.appendChild(frame);
-	 document.body.style.background = '#FCD1E5';
-  	 frame.style.width = size;
-	 frame.style.height = size;
- 	 frame.style.margin = '5% 0 0 30%';
-	 canvas.width = sizeInt;
-	 canvas.height = sizeInt;
-	 context.lineWidth = 5;
- 	 frame.appendChild(canvas);
-  	 strokeStyle = 'black';
-
-  	//var playerMode = confirm("Click 'OK' to play the computer, 'Cancel' to play with a friend");
-
-	for (i in context) {
-		context[i[0] + (i[4] || '')] = context[i];	
- 	}
+	// Centered container for responsive board
+	frame.id = 'frame';
+	document.body.appendChild(frame);
+	document.body.style.background = '#FCD1E5';
+  	frame.style.width = size;
+	frame.style.height = size;
+ 	frame.style.margin = '5% 0 0 30%';
+	canvas.width = sizeInt;
+	canvas.height = sizeInt;
+	context.lineWidth = 5;
+ 	frame.appendChild(canvas);
+  	strokeStyle = 'black';
 
 
-	with(context) {
- 		var x = sizeInt * (1 / 3);
- 	 	bn();
-	 	mT((x * 2), 0), lT((x * 2), sizeInt);
-  		mT(0, (x * 2)), lT(sizeInt, (x * 2));
-  		mT(0, x), lT(sizeInt, x);
-  	 	mT(x, 0), lT(x, sizeInt);
+ 
+
+ 	// Dynamically draw board
+ 	with(context) {
+  		
+  		var x = sizeInt * (1 / 3);
+ 	 	beginPath();
+	 	moveTo((x * 2), 0), lineTo((x * 2), sizeInt);
+  		moveTo(0, (x * 2)), lineTo(sizeInt, (x * 2));
+  		moveTo(0, x), lineTo(sizeInt, x);
+  	 	moveTo(x, 0), lineTo(x, sizeInt);
 	 	stroke();
- 	}
-
+	}
+ 
 
 
  	canvas.onclick = function(e) {
- 		var rect = canvas.getBoundingClientRect();
+ 		
+ 		var rect = canvas.getBoundingClientRect(),
 		move = ~~((e.pageY - rect.top ) / (canvas.width / 3)) * 3 +
-		 ~~((e.pageX - rect.left) / (canvas.width / 3));
-		 var x = move % 3 * (sizeInt / 3 + context.lineWidth ), y = ~~(move / 3) * (sizeInt / 3 + context.lineWidth);
- 		 
- 		 if (!board[move]) {
+		 ~~((e.pageX - rect.left) / (canvas.width / 3)),
+		x = move % 3 * (sizeInt / 3 + context.lineWidth ), 
+		y = ~~(move / 3) * (sizeInt / 3 + context.lineWidth);
+ 		
+ 		// Execute move if board space is empty 
+ 		if (!board[move]) {
+ 		 	
  		 	turn++;
 
-
- 		 	if (turn % 2 == 0) {
- 		 		loadImages('regina');
+ 		 	// Draw player image depending on turn
+ 		 	if (turn % 2 === 0) { 
+ 		 		
+ 		 		loadImages('regina', x, y);
  		 		board[move] = -1;
  		 		ref.innerHTML = comments[turn - 1];
   		 	} else {
- 		 		loadImages('cady');
+ 		 		
+ 		 		loadImages('cady', x, y);
  		 		board[move] = 1;
  		 		ref.innerHTML = comments[turn - 1];
   		 	}
 
+  		 	// Check the current status of the game
 		 	if (boardStatus() === "main") {
+ 		 		
  		 		ref.innerHTML = "Damn Africa, you won!";
-		 		setTimeout(location.reload(), 9000);
+		 		restartRound();
 		 	} else if (boardStatus() === "opp") {
+		 		
 		 		ref.innerHTML = "LOSER";
-		 		setTimeout(location.reload(), 9000);
+		 		restartRound();
 		 	} else if (boardStatus() === "draw") {
+		 		
 		 		ref.innerHTML = "DRAW";
-		 		setTimeout(location.reload(), 9000);
+		 		restartRound();
 		 	} 
 
   	}
 
 
 	function loadImages(prop) {
+			
 			var imageObj = new Image();
 			imageObj.src = sources[prop];
 			imageObj.onload = function() {
@@ -119,20 +136,20 @@ function Round(el) {
 
 
 	function boardStatus() {
-		for (combo in combos) {
-			for (var i = main = opp = 2; i >= 0; i--) {
- 					var check = combos[combo][i];
- 				if (board[check] === 1) {
-					main--;
-					console.log(main + ' this is main');
-				} else if (board[check] === -1) {
-					opp--;
-					console.log(opp + ' this is opp');
-				}
-
-				  if (opp === -1) {
+		for (combo in combos) { // Check each array in combos
+			
+			for (var i = main = opp = 0; i <= 3; i++) {
+ 					var check = combos[combo][i]; // Store each index in combo array
+ 				
+ 				if (board[check] === 1) { // Check winning index against moves on board
+					main++;
+ 				} else if (board[check] === -1) { // Check winning index against moves on board
+					opp++;
+ 				}
+				
+				if (opp === 3) {
 					return "opp";
-				} else if (main === -1) {
+				} else if (main === 3) {
 					return "main";
 				} 
 
@@ -143,8 +160,4 @@ function Round(el) {
 			return "draw";
 		}
 	}
-
-
-
-
 }
