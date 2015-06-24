a271e1251a1ee171dcd4adc2c4b59a17706aa1c8
Carron White <carronwhite@gmail.com>
2015-06-24 15:16:36 -0400

    Fixed TypeError and updated with master

--- mean-tac-toe.js
+++ mean-tac-toe.js
@@ -7,16 +7,16 @@ function Round(el) {
 
 
 
-	 var frame = document.createElement('div'),
-	 	next,
+	var frame,
+	  next,
+	 	computerMode,
 	 	size = (0.4 * window.innerWidth) + 'px',
 	 	sizeInt = parseInt(size);
 	 	marginCalc = (window.innerWidth) + '%',
 	 	canvas = document.createElement('canvas'),
 	 	context = canvas.getContext('2d'),
 	 	board = [0, 0, 0, 0, 0, 0, 0, 0],
-	 	computerMode = undefined;
-	 	ref = document.getElementsByTagName('h2')[0],
+ 	 	ref = document.getElementsByTagName('h2')[0],
 		comments = ["SO you think you’re really pretty?",
 		 "Shut up!",
 		 "You smell like a baby prostitute.",
@@ -28,20 +28,27 @@ function Round(el) {
 		 "You CAN'T sit with us!"],
 	 	sources = {
 	 		cady: 'img/cady.png',
-	 		regina: 'img/regina.png',
+	 		regina: 'img/regina.png'
  	 	},
 	 	turn = 0,
 
-	 	combos = [[0, 1, 2],
+	 	combos = [
+	 		[0, 1, 2],
 	 		[3, 4, 5], 
 		 	[6, 7, 8], 
 		 	[0, 3, 6], 
 		 	[1, 4, 7],
 		 	[2, 5, 8],
 		 	[0, 4, 8], 
-		 	[6, 4, 2]];
+		 	[6, 4, 2]
+		],
+		restartRound = function() {
+			return setTimeout(function() {
+				location.reload();
+			}, 1500);
+		};
  
-
+		frame = document.createElement('div');
 		frame.id = 'frame';
 	 	document.body.appendChild(frame);
 	 	document.body.style.background = '#FCD1E5';
@@ -54,23 +61,16 @@ function Round(el) {
  	 	frame.appendChild(canvas);
   	 	strokeStyle = 'black';
 
-  		var computerMode = confirm
-  		("Click 'OK' to play the computer, 'Cancel' to play with a friend");
+  		computerMode = confirm("Click 'OK' to play the computer, 'Cancel' to play with a friend");
 
- 	for (i in context) {
-		context[i[0] + (i[4] || '')] = context[i];	
- 	}
-
-
-
-
+ 
 	with(context) {
  		var x = sizeInt * (1 / 3);
- 	 	bn();
-	 	mT((x * 2), 0), lT((x * 2), sizeInt);
-  		mT(0, (x * 2)), lT(sizeInt, (x * 2));
-  		mT(0, x), lT(sizeInt, x);
-  	 	mT(x, 0), lT(x, sizeInt);
+ 	 	beginPath();
+	 	moveTo((x * 2), 0), lineTo((x * 2), sizeInt);
+  		moveTo(0, (x * 2)), lineTo(sizeInt, (x * 2));
+  		moveTo(0, x), lineTo(sizeInt, x);
+  	 	moveTo(x, 0), lineTo(x, sizeInt);
 	 	stroke();
  	}
 
@@ -78,14 +78,15 @@ function Round(el) {
  	if (!computerMode) {
 	 	canvas.onclick = function(e) {
 	 		var rect = canvas.getBoundingClientRect();
-			var move = ~~((e.pageY - rect.top ) / (canvas.width / 3)) * 3 +
-		 ~~((e.pageX - rect.left) / (canvas.width / 3));
-			var x = move % 3 * (sizeInt / 3 + context.lineWidth ), y = ~~(move / 3) * (sizeInt / 3 + context.lineWidth);
+			move = Math.floor((e.pageY - rect.top ) / (canvas.width / 3)) * 3 +
+		 	Math.floor((e.pageX - rect.left) / (canvas.width / 3)),
+			x = move % 3 * (sizeInt / 3 + context.lineWidth ),
+			y = Math.floor(move / 3) * (sizeInt / 3 + context.lineWidth);
 	 		 
 	 		if (!board[move]) {
-	 			turn++;
 
-	 		 	if (turn % 2 == 0) {
+	 			turn++;
+	 		 	if (turn % 2 === 0) {
 	 		 		loadImages('regina', x, y);
 	 		 		board[move] = -1;
 	 		 		ref.innerHTML = comments[turn - 1];
@@ -98,14 +99,14 @@ function Round(el) {
 
 			 	if (boardStatus() === "main") {
 	 		 		ref.innerHTML = "Damn Africa, you won!";
-			 		setTimeout(function() { location.reload()}, 1500);
+			 		restartRound();
 			 	} else if (boardStatus() === "opp") {
 			 		ref.innerHTML = "LOSER";
-			 		setTimeout(function() { location.reload()}, 9000);
+			 		restartRound();
 			 	} else if (boardStatus() === "draw") { 
 			 		//boardStatus() === "draw"; 
 			 		ref.innerHTML = "DRAW";
-			 		setTimeout(function() {location.reload()}, 9000);
+			 		restartRound();
 			 	}
 			}
 		}
@@ -113,22 +114,27 @@ function Round(el) {
 	} else {
 
 		canvas.onclick = function(e) {
-			var next;
-			var rect = canvas.getBoundingClientRect();
- 			var move = ~~((e.pageY - rect.top ) / (canvas.width / 3)) * 3 +
-				~~((e.pageX - rect.left) / (canvas.width / 3));
-			var x = move % 3 * (sizeInt / 3 + context.lineWidth ), y = ~~(move / 3) * (sizeInt / 3 + context.lineWidth);
+			
+			var next,
+			rect = canvas.getBoundingClientRect(),
+ 			move = Math.floor((e.pageY - rect.top ) / (canvas.width / 3)) * 3 +
+			Math.floor((e.pageX - rect.left) / (canvas.width / 3)),
+			x = move % 3 * (sizeInt / 3 + context.lineWidth ),
+			y = Math.floor(move / 3) * (sizeInt / 3 + context.lineWidth);
+			
 			if (!board[move]) {
-				console.log(turn);
+  					
  					loadImages('cady', x, y);
+					
 					board[move] = 1;
+					
 					turn++;
 
  					if (boardStatus(0) > 0) {
 						ref.innerHTML = "Damn Africa, you won!";
- 						setTimeout(function() { location.reload() }, 1500); 
+ 						restartRound(); 
  					} else {
-						ref.innerHTML = comments[turn - 1] /*|| comments[0]*/;
+						ref.innerHTML = comments[turn - 1];
 					}
 		 
  					next = computerMove(0, -1, (-sizeInt / 3), (sizeInt / 3));
@@ -138,7 +144,8 @@ function Round(el) {
 
 
 					x = (next % 3) * (sizeInt / 3 + context.lineWidth);
-					y = ~~(next / 3) * (sizeInt / 3 + context.lineWidth);
+					y = Math.floor(next / 3) * (sizeInt / 3 + context.lineWidth);
+					
 					setTimeout(function() {
 
 					loadImages('regina', x, y);
@@ -146,25 +153,20 @@ function Round(el) {
   					
  					if (boardStatus(0) < 0) {
  						ref.innerHTML = "LOSER";
- 						setTimeout(function() { location.reload() }, 1500); 
+ 						restartRound(); 
  					} else {
  						ref.innerHTML = comments[turn - 1];
  					}
 
  					if (next === undefined) {
  						ref.innerHTML = "DRAW";
-						setTimeout(function() { location.reload() }, 1500); 
+						restartRound(); 
 					}
 				}, 1000);
-					turn++;
-
-
-	
-					console.log(turn);
-
-	  
+				turn++;
+ 
 			}
-		}
+		};
 		
 	}
 
@@ -174,10 +176,8 @@ function Round(el) {
 			
 		imageObj.onload = function() {
 		    context.drawImage(imageObj, x, y, sizeInt / 3 - context.lineWidth * 2, sizeInt / 3 - context.lineWidth * 2);
-
 	    }
-
-  	};
+  	}
 	
 
 	function boardStatus(depth) {
@@ -192,9 +192,9 @@ function Round(el) {
 			}
 			
 			if (opp === 3) {
-				return computerMode ?   (depth - sizeInt / 3) : "opp";
+				return computerMode ? (depth - sizeInt / 3) : "opp";
  			} else if (main === 3) {
- 				return computerMode ?   (sizeInt / 3 - depth) : "main";
+ 				return computerMode ? (sizeInt / 3 - depth) : "main";
  			} 
 		}
 
@@ -204,10 +204,13 @@ function Round(el) {
 
 	};
 
-
+	// Minimax algorithm with Alpha-Beta Pruning
+	//next = computerMove(0, -1, (-sizeInt / 3), (sizeInt / 3));
 	function computerMove(depth, player, alpha, beta){
-		var i = 9, min = -sizeInt / 3, max, value, next;
-		if (value = boardStatus(depth)) {// either player won
+		var i = 9, // number of board pieces
+		min = -sizeInt / 3, max, value, next;
+		
+		if (value = boardStatus(depth)) { // calculate value from victor
 			return value * player;
 		}
   		while(i--){
@@ -215,12 +218,12 @@ function Round(el) {
  				board[i] = player;
 					 
 				value = -computerMove(depth + 1, -player, -beta, -alpha);
-				board[i] = undefined;
+				board[i] = 0;
 
 
 				if (max === undefined || value > max) {max = value;}
 				if (value > alpha) {alpha = value;}
-				if (alpha >= beta) {return alpha;} // prune branch
+				if (alpha >= beta) {return alpha;} // prune this branch
 				if (max > min){ min = max; next = i; } // best odds for next move
  			}
 		}
